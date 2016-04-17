6a63322c75c7efe9d2bba9b29b180a2ef05d6136
Joel Vasallo <jvasallo@gogoair.com>
2016-04-17 13:46:19 -0500

    Release v0.1.2 - Fixed padding on main list view to resemble Chicago flag a bit more

--- app/src/main/java/com/joelvasallo/chicagoevents/DisplayDescriptionActivity.java
+++ app/src/main/java/com/joelvasallo/chicagoevents/DisplayDescriptionActivity.java
@@ -5,12 +5,10 @@ import android.graphics.drawable.Drawable;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.text.DateFormat;
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.Date;
